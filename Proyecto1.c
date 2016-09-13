ec4e8387b3a7588fc6d87927e64b9437705ea1bd
Xiquelo <xiquelo.rosalia@gmail.com>
2016-09-13 00:22:57 -0500

    Creación del Proyecto C

---
+++ Proyecto1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <conio.h>
+#include <string.h>
+#define MAX_CONTAC 10
+#define MAX_STRING 30
+
+typedef struct contacto
+{
+	char nombre[30]
+	int telefono[10]
+} CONTACTO;
+
+void NuevoContacto()
+{
+	
+}
