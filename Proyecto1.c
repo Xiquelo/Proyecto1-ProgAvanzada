b283c924969e69cc1eb217fc1936a3a15cc56764
Xiquelo <xiquelo.rosalia@gmail.com>
2016-09-14 10:11:44 -0500

    More code

--- Proyecto1.c
+++ Proyecto1.c
@@ -2,8 +2,6 @@
 #include <stlib.h>
 #include <conio.h>
 #include <string.h>
-#define MAX_CONTAC 10
-#define MAX_STRING 30
 
 typedef struct contacto
 {
@@ -11,7 +9,58 @@ typedef struct contacto
 	int telefono[10]
 } CONTACTO;
 
-void NuevoContacto()
+void NewContact()
 {
+	CONTACTO new;
+	char select s, n;
+
+	printf("\n\t Introduzca los datos del nuevo contacto: \n");
+	scanf("%s,%d", &new.nombre, &new.telefono);
+
+	printf("\n Se ha ingresado el contacto con el nombre: %s", new.nombre);
+	printf("\n Con el no. de teléfono: %d", new.telefono);
+	printf("\n ¿Desea guardar este nuevo contacto? S/N");
+
+	if(select = s)
+		FILE *agenda;
+		agenda=fopen ("agenda.txt", "r+");
+		fseek (pf, 0, SEEK_END);
+		fprintf(agenda, "\n%s\t\t%d", new.nombre, new.telefono);
+		fclose(agenda);
+}
+
+void LoadContacts()
+{
+	FILE *agenda;
+	char c;
+
+	agenda=fopen ("agenda.txt", "r");
+	printf("\n");
+
+	while(feof(agenda)==0)
+	{
+		c=getc(agenda);
+		putchar(c);
+	}
+
+	putchar('\n');
+	fclose(agenda);
+	printf("\n\n");
+}
+
+void DelContact()
+{
+	int 1, linea, NUMlineas=0;
+	char LINEA[MAX], c, ch;
+	FILE *destino, *agenda;
 	
+	printf("\n Lista actual de contactos: \n");
+	LoadContacts();
+	printf("Introduzca el no. de contacto a eliminar: ");
+	scanf("%d", &1);
+
+	linea=1+3;
+
+	agenda=fopen("agenda.txt", "r+");
+	destino=fopen("age")
 }
